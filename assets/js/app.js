b0c16f5effff4d32eb13b1af1509ae1e07a144fc
Miguel Pedraza <mikeped469@gmail.com>
2022-10-25 12:11:16 -0500

    Added restriction to allow only 1 or 0 for binary input

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,4 +19,13 @@ const convertBinToDec = (event) => {
 }
 
 // Event listeners
+
+// Makes the convesion of binary to decimal
 convertBtn.addEventListener('click', convertBinToDec);
+
+// Checks that only 1 or 0 is entered
+inputBinaryValue.addEventListener('keyup', (event) => {        
+    if (event.key != 1 && event.key != 0) {                
+        event.target.value = event.target.value.slice(0, -1);        
+    }
+});
